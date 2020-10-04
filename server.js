a0689e92ccd7ed4277bc3b8d2fad4a2c326e31d0
Glitch (hello-express) <none>
2020-10-04 14:35:57 +0000

    🚈🎇 Checkpoint ./config.json:21995077/36 ./server.js:21995077/48

--- server.js
+++ server.js
@@ -4680,7 +4680,7 @@ var maintainloop = (() => {
         // Make base protectors if needed.
             let f = (loc, team) => { 
                 let o = new Entity(loc);
-                    o.define(Class.mothership);
+                    o.define(Class.destroyerDominator);
                     o.team = -team;
                     o.color = [10, 11, 12, 15][team-1];
             };
