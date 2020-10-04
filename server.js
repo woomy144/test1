9afb5ef03f27d270f2f9a54bc125a5c3c584befe
Glitch (project-friends) <none>
2020-10-04 14:52:36 +0000

    🐿✋ Checkpoint ./server.js:21995077/19

--- server.js
+++ server.js
@@ -4678,7 +4678,7 @@ var maintainloop = (() => {
         // Make base protectors if needed.
             let f = (loc, team) => { 
                 let o = new Entity(loc);
-                    o.define(Class.mothership);
+                    o.define(Class.PK1);
                     o.team = -team;
                     o.color = [10, 11, 12, 15][team-1];
             };
